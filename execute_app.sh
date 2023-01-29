8bde0c4bb2f65039d4c6b2b984aa55bd3f04c69f
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2023-01-29 15:07:45 -0300

    Fix: docs

--- execute_app.sh
+++ execute_app.sh
@@ -8,6 +8,6 @@ elif [ $MODE = "PUBLISH" ]; then
     ${POETRY_HOME}/bin/poetry publish --build --username "__token__" --password "${PYPI_PASSWORD}"
 elif [ $MODE = "DOCS" ]; then
     pip3 install Sphinx==6.1.3
-    python3 -m sphinx-quickstart /docs --sep --project 'Trabalho GCES 2022.2' --author 'Paulo Gonçalves Lima' -r trabalho_individual_gces_2022_paulo -l en
+    python3 -m sphinx sphinx.cmd.quickstart /docs --sep --project 'Trabalho GCES 2022.2' --author 'Paulo Gonçalves Lima' -r trabalho_individual_gces_2022_paulo -l en
     cd /docs/ && make html
 fi
