e6f94ed17e7dffe9dbf6e98967d5ef05288d6926
ci-build <ci-build@klukas.net>
2023-01-29 19:19:03 +0000

    [skip ci] Updates

--- execute_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $MODE = "RUN" ]; then
-    ${POETRY_HOME}/bin/poetry run python trabalho_individual_gces_2022_paulo/main.py
-elif [ $MODE = "TEST" ]; then
-    ${POETRY_HOME}/bin/poetry run pytest
-elif [ $MODE = "PUBLISH" ]; then
-    ${POETRY_HOME}/bin/poetry publish --build --username "__token__" --password "${PYPI_PASSWORD}"
-fi
