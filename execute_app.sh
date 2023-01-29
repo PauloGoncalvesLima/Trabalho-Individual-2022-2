1eb1f10844dc3b637cfa78f3c90e8726216b35f7
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2023-01-29 14:29:24 -0300

    Try: Docs

--- execute_app.sh
+++ execute_app.sh
@@ -2,11 +2,10 @@
 
 if [ $MODE = "RUN" ]; then
     ${POETRY_HOME}/bin/poetry run python trabalho_individual_gces_2022_paulo/main.py
-
 elif [ $MODE = "TEST" ]; then
     ${POETRY_HOME}/bin/poetry run pytest
-
 elif [ $MODE = "PUBLISH" ]; then
     ${POETRY_HOME}/bin/poetry publish --build --username "__token__" --password "${PYPI_PASSWORD}"
-
+elif [ $MODE = "DOCS" ]; then
+    ${POETRY_HOME}/bin/poetry version
 fi
